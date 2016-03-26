d5d81a5cd4a1c6eae2059f63fbde372d95a52bbc
doit05 <768068275@qq.com>
2016-03-26 20:15:13 +0800

    first commit config

---
+++ app.js
@@ -0,0 +1,45 @@
+var wechat = require('wechat');
+var config = {
+    //35b4e242c3e54c43c84b39e9a7a2935a AppSecret
+  token: 'doit05',
+  appid: 'wx11abf14c4e9143bd',
+  encodingAESKey: 'YPSJGyRHGmPvYKOs48NHkybLneO4os8RFnzwUF8Qt6W'
+};
+
+app.use(express.query());
+app.use('/wechat', wechat(config, function (req, res, next) {
+  // 微信输入信息都在req.weixin上
+  var message = req.weixin;
+  if (message.FromUserName === 'diaosi') {
+    // 回复屌丝(普通回复)
+    res.reply('hehe');
+  } else if (message.FromUserName === 'text') {
+    //你也可以这样回复text类型的信息
+    res.reply({
+      content: 'text object',
+      type: 'text'
+    });
+  } else if (message.FromUserName === 'hehe') {
+    // 回复一段音乐
+    res.reply({
+      type: "music",
+      content: {
+        title: "来段音乐吧",
+        description: "一无所有",
+        musicUrl: "http://mp3.com/xx.mp3",
+        hqMusicUrl: "http://mp3.com/xx.mp3",
+        thumbMediaId: "thisThumbMediaId"
+      }
+    });
+  } else {
+    // 回复高富帅(图文回复)
+    res.reply([
+      {
+        title: '你来我家接我吧',
+        description: '这是女神与高富帅之间的对话',
+        picurl: 'http://nodeapi.cloudfoundry.com/qrcode.jpg',
+        url: 'http://nodeapi.cloudfoundry.com/'
+      }
+    ]);
+  }
+}));
