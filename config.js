56143c1b2b688537fc2eace86c73ba0c92bafd6f
768068275@qq.com <768068275@qq.com>
2016-03-27 14:10:40 +0800

    delete wechat

---
+++ config.js
@@ -0,0 +1,7 @@
+var config = {
+    //35b4e242c3e54c43c84b39e9a7a2935a AppSecret
+  token: 'doit05',
+  appid: 'wx11abf14c4e9143bd',
+  encodingAESKey: 'YPSJGyRHGmPvYKOs48NHkybLneO4os8RFnzwUF8Qt6W'
+};
+module.exports = config;
